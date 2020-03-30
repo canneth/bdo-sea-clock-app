c5c38fd82c39d808f9b109775bce799d09281ed5
CHJrWithaC <CHJrWithaC@gmail.com>
2020-03-30 12:27:45 +0800

    Fixed periodic notifications

--- app/src/main/java/com/example/bdoseatime/MainActivity.kt
+++ app/src/main/java/com/example/bdoseatime/MainActivity.kt
@@ -1,9 +1,6 @@
 package com.example.bdoseatime
 
-import android.app.Notification
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.app.PendingIntent
+import android.app.*
 import android.content.Intent
 import android.graphics.Color
 import android.os.Build
@@ -21,6 +18,8 @@ import java.time.temporal.ChronoUnit
 
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var notification_manager: NotificationManager
 
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/example/bdoseatime/NotifClockService.kt
+++ app/src/main/java/com/example/bdoseatime/NotifClockService.kt
@@ -5,6 +5,7 @@ import android.app.NotificationManager
 import android.app.PendingIntent
 import android.app.Service
 import android.content.Intent
+import android.graphics.Color
 import android.os.Build
 import android.os.IBinder
 import android.util.Log
@@ -53,17 +54,32 @@ class NotifClockService : Service() {
 	@RequiresApi(Build.VERSION_CODES.O)
 	override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
 
-		// Create notification channel
+		// Create service (main) notification channel for the persistent notification indicating app running in foreground
 		notification_manager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
 		val channel_id = "com.example.bdoseatime"
 		val channel_name = "main_notif_channel"
 		val channel_description = "main_notif_description"
 		val channel_importance = NotificationManager.IMPORTANCE_HIGH
-		var channel = NotificationChannel(channel_id, channel_name, channel_importance)
+		val channel = NotificationChannel(channel_id, channel_name, channel_importance)
+		channel.description = channel_description
+		channel.enableVibration(false)
+		channel.enableLights(false)
+		// Create periodic notification channel for notifications upon day/night transitions in BDO
+		val channel_periodic_id = "com.example.bdoseatime_periodic"
+		val channel_periodic_name = "periodic_notif_channel"
+		val channel_periodic_description = "main_notif_description"
+		val channel_periodic_importance = NotificationManager.IMPORTANCE_HIGH
+		val channel_periodic = NotificationChannel(channel_periodic_id, channel_periodic_name, channel_periodic_importance)
 		channel.description = channel_description
 		channel.enableVibration(true)
-		notification_manager.createNotificationChannel(channel) // Registers channel with manager
-		val builder = NotificationCompat.Builder(this, channel_id) // Create builder for channel
+		channel.enableLights(true)
+		channel.lightColor = Color.CYAN
+		// Register channels with manager
+		notification_manager.createNotificationChannel(channel)
+		notification_manager.createNotificationChannel(channel_periodic)
+		// Create builder for the channels
+		val builder = NotificationCompat.Builder(this, channel_id)
+		val builder_periodic = NotificationCompat.Builder(this, channel_periodic_id)
 		// Intents for notifications
 		val intent = Intent(applicationContext, MainActivity::class.java)
 		val pending_intent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
@@ -90,6 +106,8 @@ class NotifClockService : Service() {
 		val NIGHT = false
 		var last_state = DAY
 
+		var flip_flop = true
+
 		val runnable = Runnable {
 			while(true) {
 				// Grab time from phone
@@ -113,14 +131,20 @@ class NotifClockService : Service() {
 						// Calculate current_bdo_time
 						current_bdo_time = reference_bdo_time.plusSeconds((remainder * 15 * 60 / 200))
 						if (last_state == NIGHT) {
-							// Cancel previous notification
-							stopForeground(true)
+							// Build notification
+							builder_periodic.setContentTitle("It's a new day!")
+							builder_periodic.setSmallIcon(R.mipmap.ic_launcher_round)
+							builder_periodic.setContentIntent(pending_intent)
+							builder_periodic.setOnlyAlertOnce(true)
+							builder_periodic.setAutoCancel(true)
+							// Send notification
+							notification_manager.notify(2, builder_periodic.build())
 						} else {
-							builder.setContentTitle("It's a new day!")
+							builder.setContentTitle("Day Time (^ _ ^ )")
 							// Build notification
 							val formatted_current_bdo_time = current_bdo_time.format(time_formatter)
-							builder.setContentText("It is $formatted_current_bdo_time (day time) now in BDO SEA!")
-							builder.setSmallIcon(R.mipmap.ic_launcher)
+							builder.setContentText("It is $formatted_current_bdo_time now in BDO SEA")
+							builder.setSmallIcon(R.mipmap.ic_launcher_round)
 							builder.setContentIntent(pending_intent)
 							builder.setOngoing(true)
 							builder.setOnlyAlertOnce(true)
@@ -133,14 +157,20 @@ class NotifClockService : Service() {
 						// Calculate current_bdo_time
 						current_bdo_time = bdo_night_time.plusSeconds(((remainder - 200 * 60) * 9 * 60 / 40))
 						if (last_state == DAY) {
-							// Cancel previous notification
-							stopForeground(true)
+							// Build notification
+							builder_periodic.setContentTitle("Night has fallen!")
+							builder_periodic.setSmallIcon(R.mipmap.ic_launcher_round)
+							builder_periodic.setContentIntent(pending_intent)
+							builder_periodic.setOnlyAlertOnce(true)
+							builder_periodic.setAutoCancel(true)
+							// Send notification
+							notification_manager.notify(2, builder_periodic.build())
 						} else {
 							// Build notification
-							builder.setContentTitle("Night has fallen!")
+							builder.setContentTitle("Night Time (u _ u )")
 							val formatted_current_bdo_time = current_bdo_time.format(time_formatter)
-							builder.setContentText("It is $formatted_current_bdo_time (night time) now in BDO SEA!")
-							builder.setSmallIcon(R.mipmap.ic_launcher)
+							builder.setContentText("It is $formatted_current_bdo_time now in BDO SEA")
+							builder.setSmallIcon(R.mipmap.ic_launcher_round)
 							builder.setContentIntent(pending_intent)
 							builder.setOngoing(true)
 							builder.setOnlyAlertOnce(true)
